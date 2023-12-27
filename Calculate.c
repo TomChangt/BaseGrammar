86b82e9e7730b2536909d121d7307cd4a39f3695
畅通 <>
2023-12-28 07:28:16 +0800

    commit calculate

--- Calculate.c
+++ Calculate.c
@@ -6,7 +6,9 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define FILEPATH "calculate.txt"
+#define FILEPATH "Calculate.txt"
+#define MAXCOUNT 100
+#define RANDNUM 10000
 
 FILE *operationFile(char *mode) {
     FILE *fileData = fopen(FILEPATH, mode);
@@ -18,6 +20,7 @@ FILE *operationFile(char *mode) {
 }
 
 void initData() {
+
     srand((unsigned int) time(NULL)); // 初始化随机数生成器
 
     FILE *fileData = operationFile("w+");
@@ -27,14 +30,36 @@ void initData() {
         exit(EXIT_FAILURE);
     }
 
+    int line = rand() % MAXCOUNT + 1;
 
+    char buffer[128] = "";
+    for (int i = 0; i < line; ++i) {
+        char operator = '+';
+        int a = rand() % RANDNUM + 1;
+        int b = rand() % RANDNUM + 1;
+        int c = rand() % 4;
+        switch (c) {
+            case 1:
+                operator = '-';
+                break;
+            case 2:
+                operator = '*';
+                break;
+            case 3:
+                operator = '/';
+                break;
+        }
+        sprintf(buffer,"%d %c %d =\n",a,operator,b);
+        fputs(buffer, fileData);
+    }
+    fclose(fileData);
 
 }
 
 
 int main() {
 
-    printf("hello world");
+    initData();
 
     return 0;
 }
